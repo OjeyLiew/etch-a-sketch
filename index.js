14e2feea6074e5651dc966699703daab97d3b14e
Joey <joey@p200300eac7077d499118aa9d245eff89.dip0.t-ipconnect.de>
2025-12-25 20:46:51 +0100

    Add button to create dynamic size grid

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-const grid = document.createElement("div");
 const body = document.querySelector("body");
-grid.classList.add("grid");
-for (let i = 0; i < 16*16; i++) {
-	const square = document.createElement("div");
-	square.classList.add("square");
-	square.addEventListener("mouseover", (event) => {
-		square.style.backgroundColor = "red";
-	})
-	grid.appendChild(square);
+
+
+// Ask user with prompt the size of the grid
+const newGrid = document.createElement("button");
+newGrid.textContent = "New Grid";
+newGrid.addEventListener("click", (event) => {
+	let gridSize = -1;
+	do {
+		gridSize = parseInt(prompt("Enter the size of the grid: ", -1));
+	} while(gridSize < 0 || gridSize > 100);
+	const grid = document.querySelector(".grid");
+	grid.remove();
+	createGrid(gridSize);
+})
+
+body.appendChild(newGrid);
+createGrid(16);
+
+// Create new grid
+function createGrid(size) {
+	const grid = document.createElement("div");
+	grid.classList.add("grid");
+	for (let i = 0; i < size*size; i++) {
+		const square = document.createElement("div");
+		square.classList.add("square");
+		calcSize = window.innerWidth/size;
+		square.style.width = `${calcSize}px`;
+		square.style.height = `${calcSize}px`;
+		square.addEventListener("mouseover", (event) => {
+			square.style.backgroundColor = "red";
+		})
+		grid.appendChild(square);
+	}
+	body.appendChild(grid);
 }
-body.appendChild(grid);
+
+console.log(window.innerWidth);
