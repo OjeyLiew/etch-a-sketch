777996d3fb5e6bd092006673eda0eaf3f3c5705e
Joey <joey@p200300eac7077d499118aa9d245eff89.dip0.t-ipconnect.de>
2025-12-25 19:16:26 +0100

    Hover over the grid element changes its background color

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ grid.classList.add("grid");
 for (let i = 0; i < 16*16; i++) {
 	const square = document.createElement("div");
 	square.classList.add("square");
+	square.addEventListener("mouseover", (event) => {
+		square.style.backgroundColor = "red";
+	})
 	grid.appendChild(square);
 }
 body.appendChild(grid);
